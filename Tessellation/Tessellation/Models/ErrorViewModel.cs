34e30947c4fe61a093b45cb80548f0570273881b
kruppatomi <hungaryca@gmail.com>
2020-06-18 21:07:14 +0200

    new MVC project was created

---
+++ Tessellation/Tessellation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tessellation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
