e5713cd9b537b8276c8710ef21f531c683b926e6
kruppatomi <hungaryca@gmail.com>
2020-06-19 00:45:22 +0200

    insert username and password to db

--- Tessellation/Tessellation/Controllers/HomeController.cs
+++ Tessellation/Tessellation/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Tessellation.Models;
 
@@ -12,10 +14,12 @@ namespace Tessellation.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
         {
             _logger = logger;
+            _configuration = config;
         }
 
         public IActionResult Index()
@@ -23,9 +27,22 @@ namespace Tessellation.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Page2(User user)
         {
-            return View();
+            //get the user and send it to the db
+
+            //connect to the db
+            string connectionString = _configuration.GetConnectionString("MyConnectionString");
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            string query = "INSERT INTO [dbo].[tessellation_users]([name],[password]) VALUES(@name, @password)";
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@name", user.Name);
+            sqlCommand.Parameters.AddWithValue("@password", user.Password);
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            return View(user);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
