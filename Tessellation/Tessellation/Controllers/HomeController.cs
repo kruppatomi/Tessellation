9fd48bad65730bd5f0bf80a8b3dd123fd3902416
kruppatomi <hungaryca@gmail.com>
2020-06-20 17:24:38 +0200

    hide password and recommend it

--- Tessellation/Tessellation/Controllers/HomeController.cs
+++ Tessellation/Tessellation/Controllers/HomeController.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Tessellation.Models;
 using Tessellation.Services;
-using BCrypt.Net;
 
 namespace Tessellation.Controllers
 {
