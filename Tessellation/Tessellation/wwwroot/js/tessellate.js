bbc73658d29d006741b9ad7ee683611e4a43fe04
kruppatomi <hungaryca@gmail.com>
2020-08-07 08:40:08 +0200

    almost nailed point flipping

--- Tessellation/Tessellation/wwwroot/js/tessellate.js
+++ Tessellation/Tessellation/wwwroot/js/tessellate.js
@@ -1,6 +1,6 @@
 ﻿
 var tessellationWidth;
-var tessellationHeigth;
+var tessellationHeight;
 var tessellationContext;
 var tessellationSites = [];
 var tessellationVoronoi;
@@ -41,55 +41,109 @@ function drawforSave() {
 
 function calculateMirroredPoints() {
 
+    let bf;
+    let jf;
+    let ba;
+    let ja;
+
+    if (tessellationWidth % 2 == 0 && tessellationHeight % 2 == 0) {
+        bf = (tessellationWidth / 2) * (tessellationHeight / 2);
+        jf = bf;
+        ba = bf;
+        ja = bf;
+
+    }
+    if (tessellationWidth % 2 != 0 && tessellationHeight % 2 == 0) {
+        bf = ((Math.floor(tessellationWidth / 2) + 1) * tessellationHeight / 2);
+        jf = (Math.floor(tessellationWidth / 2) * tessellationHeight / 2);
+        ba = ((Math.floor(tessellationWidth / 2) + 1) * tessellationHeight / 2);
+        ja = ((Math.floor(tessellationWidth / 2)) * tessellationHeight / 2);
+    }
+    if (tessellationWidth % 2 != 0 && tessellationHeight % 2 != 0) {
+        bf = ((Math.floor(tessellationWidth / 2) + 1) * (Math.floor(tessellationHeight / 2) + 1));
+        jf = (Math.floor(tessellationWidth / 2) * (Math.floor(tessellationHeight / 2) + 1));
+        ba = ((Math.floor(tessellationWidth / 2) + 1) * (Math.floor(tessellationHeight / 2)));
+        ja = ((Math.floor(tessellationWidth / 2) + 1) * (Math.floor(tessellationHeight / 2)));
+    }
+    if (tessellationWidth % 2 == 0 && tessellationHeight % 2 != 0) {
+        bf = (tessellationWidth / 2) * (Math.floor(tessellationHeight / 2) + 1);
+        jf = (tessellationWidth / 2) * (Math.floor(tessellationHeight / 2) + 1);
+        ba = ((Math.floor(tessellationWidth / 2) + 1) * (Math.floor(tessellationHeight / 2)));
+        ja = (tessellationWidth / 2) * (Math.floor(tessellationHeight / 2));
+    }
     tessellationSites = [];
+
+    for (let i = bf; i > 0; i--) {
+        for (let j = 0; j < sites.length; j++) {
+            tessellationSites.push([500 * (i - 1) + sites[j][0], sites[j][1]]);
+        }
+    }
+    for (let i = jf; i > 0; i--) {
+        for (let j = 0; j < sites.length; j++) {
+            tessellationSites.push([500 * (i+1) - sites[j][0], sites[j][1]]);
+        }
+    }
+    for (let i = ba; i > 0; i--) {
+        for (let j = 0; j < sites.length; j++) {
+            tessellationSites.push([sites[j][0], 500 * (i+1) - sites[j][1]]);
+        }
+    }
+    for (let i = ja; i > 0; i--) {
+        for (let j = 0; j < sites.length; j++) {
+            tessellationSites.push([500 * i - sites[j][0], 500 * (i+1) - sites[j][1]]);
+        }
+    }
+
+    //for (let i = 0; i < sites.length; i++) {
+    //    //Sima
+    //    tessellationSites.push(sites[i]);
+    //    //balalso
+    //    tessellationSites.push([sites[i][0], canvasToSave.height - sites[i][1]]);
+    //    //jobb
+    //    tessellationSites.push([canvasToSave.width - sites[i][0], sites[i][1]]);
+    //    //jobbalso
+    //    tessellationSites.push([canvasToSave.width - sites[i][0], canvasToSave.height - sites[i][1]]);
+    //}
+
     //ha osztható 2 vel akkor... nezzuk meg mi lenne ha 4x4 lenne
     //width
-    for (let j = tessellationWidth; j > 0; j--) {
-        if (j % 2 == 0) {
-            //jobb
-            tessellationSites.push([tessellationWidth * j - sites[i][0], sites[i][1]]);
-        }
-        if (j == 1) {
-            for (let i = 0; i < sites.length; i++) {
-                //Sima
-                tessellationSites.push(sites[i]);
-            }
-        }
-        if (j % 2 != 0 && j != 1) {
-            for (let i = 0; i < sites.length; i++) {
-                //Sima
-                tessellationSites.push((tessellationWidth * (j-1)) + sites[i][0], sites[i][1]);
-            }
-        }
-    }
+    //for (let j = tessellationWidth; j > 0; j--) {
+    //    if (j % 2 == 0) {
+    //        //jobb
+    //        tessellationSites.push([tessellationWidth * j - sites[i][0], sites[i][1]]);
+    //    }
+    //    if (j == 1) {
+    //        for (let i = 0; i < sites.length; i++) {
+    //            //Sima
+    //            tessellationSites.push(sites[i]);
+    //        }
+    //    }
+    //    if (j % 2 != 0 && j != 1) {
+    //        for (let i = 0; i < sites.length; i++) {
+    //            //Sima
+    //            tessellationSites.push((tessellationWidth * (j-1)) + sites[i][0], sites[i][1]);
+    //        }
+    //    }
+    //}
 
-    //height
-    for (let k = tessellationHeight; k > 0; k--) {
-        if (k % 2 == 0) {
+    ////height
+    //for (let k = tessellationHeight; k > 0; k--) {
+    //    if (k % 2 == 0) {
 
-        }
-        if (k % 2 != 0) {
-            //balalso
-            tessellationSites.push([sites[i][0], tessellationHeigth - sites[i][1]]);
-        }
-        
-        for (let i = 0; i < sites.length; i++) {
-            //Sima
-            tessellationSites.push(sites[i]);
-            //balalso
-            tessellationSites.push([sites[i][0], canvasToSave.height - sites[i][1]]);
-            //jobb
-            tessellationSites.push([canvasToSave.width - sites[i][0], sites[i][1]]);
-            //jobbalso
-            tessellationSites.push([canvasToSave.width - sites[i][0], canvasToSave.height - sites[i][1]]);
-        }
-    }
+    //    }
+    //    if (k % 2 != 0) {
+    //        //balalso
+    //        tessellationSites.push([sites[i][0], tessellationHeigth - sites[i][1]]);
+    //    }
+
+
 }
+
 
 
 function initialise() {
     tessellationWidth = document.getElementById('tWidth').value;
-    tessellationHeigth = document.getElementById('tHeight').value;
+    tessellationHeight = document.getElementById('tHeight').value;
 
     let tessellationConteiner = document.getElementById('pictureContainer');
     if (document.getElementById('picture') == null) {
@@ -102,7 +156,7 @@ function initialise() {
     canvasToSave = document.createElement('canvas');
     canvasToSave.id = "tessellationCanvas"
     canvasToSave.width = 500 * tessellationWidth;
-    canvasToSave.height = 500 * tessellationHeigth;
+    canvasToSave.height = 500 * tessellationHeight;
     pDiv.appendChild(canvasToSave);
     tessellationConteiner.appendChild(pDiv);
 
